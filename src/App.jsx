df616f6a535753da2c1cdfda4ae77c6ee2e53499
yaredghebre <yared2391@gmail.com>
2023-12-11 15:42:52 +0100

    router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react';
+
+// Router-dom
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import AddNewPost from './pages/AddNewPost';
+import About from './pages/About';
+import SinglePost from './pages/SinglePost';
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="ms_wrapper">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/:slug" element={<SinglePost />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/add-new-post" element={<AddNewPost />} />
+        </Routes>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   );
 }
 
